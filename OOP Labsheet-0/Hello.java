2cc2fde2d6b46d718607ce8a52107bd8ee59212d
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2022-02-07 19:59:53 +0530

    commit

---
+++ OOP Labsheet-0/Hello.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of Hello here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
